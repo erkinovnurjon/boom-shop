d69dab876c3cf0d4976058a1dfce3bcfad76edf7
Nurjon <nurjon2477@gmail.com>
2023-12-18 19:48:45 +0500

    handlebars

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import express from "express"
 import path , {dirname} from "path"
 import {fileURLToPath} from "url"
+import {engine} from "express-handlebars"
 
 const app = express()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+app.engine('handlebars' , engine())
+app.set('view engine' , 'handlebars')
+app.set('views' , './views')
+
 app.get('/' , (req, res) => {
-      res.sendFile(path.join(__dirname , 'views' , 'index.html'))
+      res.render('index')
 })
 
 app.get('/about' , (req, res) => {
-      res.sendFile(path.join(__dirname , 'views' , 'about.html'))
+      res.render('about')
 })
 
 
