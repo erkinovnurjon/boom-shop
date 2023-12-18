9a51475bbf444c734b6e959b48f52686b760ce83
Nurjon <nurjon2477@gmail.com>
2023-12-18 18:31:50 +0500

    rounting for node.js basic

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import express from "express"
+import path , {dirname} from "path"
+import {fileURLToPath} from "url"
 
 const app = express()
-const PORT = process.env.PORT  || 4100
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+app.get('/' , (req, res) => {
+      res.sendFile(path.join(__dirname , 'views' , 'index.html'))
+})
+
+app.get('/about' , (req, res) => {
+      res.sendFile(path.join(__dirname , 'views' , 'about.html'))
+})
+
+
+
+const PORT = process.env.PORT  || 4100
 app.listen(4100 , () => console.log(`Server is running on port : ${PORT}`))
