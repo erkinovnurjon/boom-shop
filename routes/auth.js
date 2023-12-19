0d371e9ec213d77114b1b16fb07b422e66887407
Nurjon <nurjon2477@gmail.com>
2023-12-19 16:28:56 +0500

    dynamic title

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,17 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/login', (req, res) => {
-      res.render('login')
+      res.render('login' , {
+            title : "Login ",
+            isLogin : true
+      })
 })
 
 router.get('/register', (req, res) => {
-      res.render('register')
+      res.render('register' , {
+            title : "Register ",
+            isRegister : true
+      })
 })
 
 export default router
--- routes/products.js
+++ routes/products.js
@@ -3,16 +3,24 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/', (req, res) => {
-      res.render('index')
+      res.render('index' , {
+            title : "Boom shop "
+      })
 })
 
 
 router.get('/products', (req, res) => {
-      res.render('products')
+      res.render('products' , {
+            title : "Products  shop",
+            isProducts:true
+      })
 })
 
 router.get('/add', (req, res) => {
-      res.render('add')
+      res.render('add' , {
+            title : "Add product",
+            isAdd : true
+      })
 })
 
 export default router
