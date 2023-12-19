fc7e2498275f8f503fd72eb75a34d4b8e950ab46
Nurjon <nurjon2477@gmail.com>
2023-12-19 18:54:38 +0500

    satic folder

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const hbs = create ({
 })
 
 app.engine('hbs' , hbs.engine)
+
 app.set('view engine' , 'hbs')
 app.set('views' , './views')
+
 app.use(express.urlencoded({extended : true}))
+app.use(express.static("public"))
 
 app.use(authRoutes)
 app.use(productsRoutes)
