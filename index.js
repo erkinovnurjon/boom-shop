003eefad24a7823eeba10ab759194233f6b60dda
Nurjon <nurjon2477@gmail.com>
2023-12-20 18:47:56 +0500

    validation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { create } from 'express-handlebars';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
+import flash from "connect-flash"
+import session from 'express-session';
 
 // ROUTES
 import AuthRoutes from './routes/auth.js';
@@ -20,6 +22,8 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({ secret: 'Nurik', resave: false, saveUninitialized: false }))
+app.use(flash())
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,78 @@
-import { Router } from "express";
-import User from "../models/user.js";
-import bcrypt from "bcrypt"
+import { Router } from 'express'
+import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 const router = Router()
 
 router.get('/login', (req, res) => {
-      res.render('login' , {
-            title : "Login ",
-            isLogin : true
+      res.render('login', {
+            title: 'Login | Sammi',
+            isLogin: true,
+            loginError: req.flash('loginError'),
       })
 })
 
 router.get('/register', (req, res) => {
-      res.render('register' , {
-            title : "Register ",
-            isRegister : true
+      res.render('register', {
+            title: 'Register | Sammi',
+            isRegister: true,
+            registerError: req.flash('registerError'),
       })
 })
 
-router.post('/register' , async (req , res) => {
-      const hashPassword = await bcrypt.hash(req.body.password , 10)
-      const userData = {
-            firstName : req.body.firstname,
-            lastName : req.body.lastname,
-            email : req.body.email ,
-            password : hashPassword
+router.post('/login', async (req, res) => {
+      const { email, password } = req.body
+
+      if (!email || !password) {
+            req.flash('loginError', 'All fields is required')
+            res.redirect('/login')
+            return
       }
-      
+
+      const existUser = await User.findOne({ email })
+      if (!existUser) {
+            req.flash('loginError', 'User not found')
+            res.redirect('/login')
+            return
+      }
+
+      const isPassEqual = await bcrypt.compare(password, existUser.password)
+      if (!isPassEqual) {
+            req.flash('loginError', 'Password wrong')
+            res.redirect('/login')
+            return
+      }
+
+      console.log(existUser)
+      res.redirect('/')
+})
+
+router.post('/register', async (req, res) => {
+      const { firstname, lastname, email, password } = req.body
+
+      if (!firstname || !lastname || !email || !password) {
+            req.flash('registerError', 'All fields is required')
+            res.redirect('/register')
+            return
+      }
+
+      const candidate = await User.findOne({ email })
+
+      if (candidate) {
+            req.flash('registerError', 'User already exist')
+            res.redirect('/register')
+            return
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10)
+      const userData = {
+            firstName: firstname,
+            lastName: lastname,
+            email: email,
+            password: hashedPassword,
+      }
       const user = await User.create(userData)
-      console.log(user);
       res.redirect('/')
 })
-
-router.post('/login' , (req , res) => {
-      console.log(req.body)
-      res.redirect('/')
-})
-
 
 export default router
