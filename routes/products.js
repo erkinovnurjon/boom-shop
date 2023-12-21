69115174d025f2ef196f88244535320ec0ece0a2
Nurjon <nurjon2477@gmail.com>
2023-12-21 22:10:28 +0500

    edit product

--- routes/products.js
+++ routes/products.js
@@ -1,57 +1,81 @@
-import { Router } from "express";
-import Product from "../models/Product.js";
-import authMiddleware from "../middleware/auth.js";
-import userMiddleware from "../middleware/user.js";
+import { Router } from 'express'
+import authMiddleware from '../middleware/auth.js'
+import userMiddleware from '../middleware/user.js'
+import Product from '../models/Product.js'
 
 const router = Router()
 
 router.get('/', async (req, res) => {
       const products = await Product.find().lean()
-      res.render('index' , {
-            title : "Boom shop ",
-            products : products.reverse(),
-            userId : req.userId ? req.userId.toString() : null
+
+      res.render('index', {
+            title: 'Boom shop | Sammi',
+            products: products.reverse(),
+            userId: req.userId ? req.userId.toString() : null,
       })
 })
 
-
-router.get('/products', async  (req, res) => {
+router.get('/products', async (req, res) => {
       const user = req.userId ? req.userId.toString() : null
-      const myProduct = await Product.find({user}).populate('user').lean()
-      res.render('products' , {
-            title : "Products  shop",
-            isProducts:true,
-            myProducts : myProduct
+      const myProducts = await Product.find({ user }).populate('user').lean()
+
+      res.render('products', {
+            title: 'Products | Sammi',
+            isProducts: true,
+            myProducts: myProducts,
       })
 })
 
-router.get('/add', authMiddleware , (req, res) => {
-      
-      res.render('add' , {
-            title : "Add product",
-            isAdd : true,
-            errorAddProducts: req.flash('errorAddProducts')
+router.get('/add', authMiddleware, (req, res) => {
+      res.render('add', {
+            title: 'Add products',
+            isAdd: true,
+            errorAddProducts: req.flash('errorAddProducts'),
       })
 })
 
-router.get('/product/:id' , async (req,res) => {
+router.get('/product/:id', async (req, res) => {
       const id = req.params.id
-      const product  = await Product.findById(id).populate('user').lean()
-      res.render('product' , {
-            product : product,
+      const product = await Product.findById(id).populate('user').lean()
+
+      res.render('product', {
+            product: product,
       })
 })
 
-router.post('/add-products' , userMiddleware , async (req,res) => {
-      const {title , description , image , price} = req.body
+router.get('/edit-product/:id', async (req, res) => {
+      const id = req.params.id
+      const product = await Product.findById(id).populate('user').lean()
+
+      res.render('edit-product', {
+            product: product,
+            errorEditProduct: req.flash('errorEditProduct'),
+      })
+})
+
+router.post('/add-products', userMiddleware, async (req, res) => {
+      const { title, description, image, price } = req.body
       if (!title || !description || !image || !price) {
             req.flash('errorAddProducts', 'All fields is required')
             res.redirect('/add')
             return
       }
 
-       await Product.create({...req.body , user : req.userId})
+      await Product.create({ ...req.body, user: req.userId })
       res.redirect('/')
+})
+
+router.post('/edit-product/:id', async (req, res) => {
+      const { title, description, image, price } = req.body
+      const id = req.params.id
+      if (!title || !description || !image || !price) {
+            req.flash('errorEditProduct', 'All fields is required')
+            res.redirect(`/edit-product/${id}`)
+            return
+      }
+
+      await Product.findByIdAndUpdate(id, req.body, { new: true })
+      res.redirect('/products')
 })
 
 export default router
