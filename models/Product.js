fbddbaadd59b61cc3828f891fa8b0000e4d17adc
Nurjon <nurjon2477@gmail.com>
2023-12-21 08:28:20 +0500

    add product oage

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+
+const ProductSchema = new Schema({
+      title: { type: String, required: true },
+      description: { type: String, required: true },
+      image: { type: String, required: true, unique: true },
+      price: { type: Number, required: true },
+      user : {type : Schema.Types.ObjectId , ref : 'User'}
+} , {timestamps : true})
+
+const Product = model('Product', ProductSchema)
+export default Product
